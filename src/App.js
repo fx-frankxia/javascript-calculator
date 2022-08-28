afbc321377e566c7173b8dc500d974174674e74c
Frank Xia <fx.frankxia@gmail.com>
2022-08-28 10:08:22 +0800

    basic css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div id="display">
+          <div id='input'></div>
+          <div id='output'></div>
+      </div>
+      <div id='container-keys'>
+        <div id="clear">AC</div>
+        <div id="divide">/</div>
+        <div id="multiply">X</div>
+        <div id="seven">7</div>
+        <div id="eight">8</div>
+        <div id="nine">9</div>
+        <div id="subtract">-</div>
+        <div id="four">4</div>
+        <div id="five">5</div>
+        <div id="six">6</div>
+        <div id="add">+</div>
+        <div id="one">1</div>
+        <div id="two">2</div>
+        <div id="three">3</div>
+        <div id="equals">=</div>
+        <div id="zero">0</div>
+        <div id="decimal">.</div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
